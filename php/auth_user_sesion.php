6e59214b8b20d74d3cdc17b56a77fe5a82b57116
Joaquin Ferrer Benzegga <43266@a.iesleopoldoqueipo.com>
2021-12-14 16:54:28 +0100

    Auth User Session

--- php/auth_user_sesion.php
+++ php/auth_user_sesion.php
@@ -9,43 +9,54 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
     session_start();
     include('dbconnection.php');
     $ultimo_login = null;
-    echo "Comprobando conexión a base de datos...";
+    echo "Comprobando conexión a base de datos... <br/>";
 
     $sql = "SELECT password FROM agenda WHERE username = :user";
     $stmt = $conn->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
     $stmt->execute(array(':user' => $_SERVER['PHP_AUTH_USER']));
     $usuario = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
     if(password_verify($_SERVER['PHP_AUTH_PW'], $usuario[0]["password"])) {
-        $_SESSION['usuario'] = $_SERVER['PHP_AUTH_USER'];
+
+        if (isset($_POST['limpiar'])) {
+            unset($_SESSION['visita']);
+            unset($_POST['limpiar']);
+            $_SESSION['visita'] = array();
+        } else {
+            ?>
+            <form id='vaciar' action='<?php echo $_SERVER['PHP_SELF'];?>' method='post'>
+                <input type='submit' name='limpiar' value='Limpiar registro'/>
+            </form>
+            <?php
+            $_SESSION['visita'][] = time();
+        }
+
+        echo "Nombre de usuario: ".$_SERVER['PHP_AUTH_USER']."<br />";
+        echo "Hash de la contraseña: ".($_SERVER['PHP_AUTH_PW'])."<br />";
+        if (count($_SESSION['visita']) == 0)
+            echo "Bienvenido. Esta es su primera visita.";
+        else {
+            date_default_timezone_set('Europe/Madrid');
+            foreach($_SESSION['visita'] as $v)
+                echo date("d/m/y \a \l\a\s H:i", $v) . "<br />";
+        }
     } else {
         header('WWW-Authenticate: Basic realm="Contenido restringido"');
         header("HTTP/1.0 401 Unauthorized");
         exit;
     }
+
+    // if (isset($ultimo_login))
+    //     echo "Ultimo login: " . date("d/m/y \a \l\a\s H:i", $ultimo_login);
+    // else
+    //     echo "Bienvenido. Esta es su primera visita.";
+
     $conn = null;
 
-    if (isset($_POST['limpiar']))
-        unset($_SESSION['visita']);
-    else
-        $_SESSION['visita'][] = time();
-
-
-    if (count($_SESSION['visita'] == 0)) {
-        echo "Bienvenido. esta es su primera visita";
-    } else {
-        date_default_timezone_set('Europe/Madrid');
-        foreach($_SESSION['visita'] as $v)
-        echo date("d/m/y \a \l\a\s H:i", $v) . "<br />";
-    }
-    ?>
-        <form id='vaciar' action='<?php echo $_SERVER['PHP_SELF'];?>' method='post'>
-            <input type='submit' name='limpiar' value='Limpiar registro'/>
-        </form>
-    <?php
-} else
-echo "Se ha producido el error $error.<br />";
-?>
+    // $ncount = $stmt->rowCount();
+    // echo $ncount;
+}
 
 // echo "Nombre de usuario: ".$_SERVER['PHP_AUTH_USER']."<br />";
 // echo "Contraseña: ".$_SERVER['PHP_AUTH_PW']."<br />";
